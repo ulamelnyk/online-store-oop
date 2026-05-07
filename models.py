d00ddab0db4c71cc6fc3ddf2ed31a5daa7f1e17d
ulianamelnyk <melnykula23@gmail.com>
2026-05-08 00:56:05 +0200

    Improved code style and changed class Order

--- models.py
+++ models.py
@@ -1,14 +1,20 @@
 class Product:
-    def __init__(self, name, price, quantity, category):
+    def __init__(
+            self,
+            name: str,
+            price: float,
+            quantity: int,
+            category: str
+    ) -> None:
         self.name = name
         self.price = price
         self.quantity = quantity
         self.category = category
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.name} - {self.price}"
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return{
             "name": self.name,
             "price": self.price,
@@ -17,7 +23,7 @@ class Product:
         }
 
     @staticmethod
-    def from_dict(data):
+    def from_dict(data: dict) -> "Product":
         return Product(
             data["name"],
             data["price"],
@@ -26,47 +32,88 @@ class Product:
         )
 
 class Cart:
-    def __init__(self):
+    def __init__(self) -> None:
         self.items = []
 
-    def add_product(self, product):
+    def add_product(self, product: Product) -> None:
         self.items.append(product)
 
-    def remove_product(self, product):
+    def remove_product(self, product: Product) -> None:
         if product in self.items:
             self.items.remove(product)
 
-    def get_total(self):
+    def get_total(self) -> float:
         return sum(product.price for product in self.items)
 
 
 class Order:
-    def __init__(self, products, total):
+    def __init__(
+            self,
+            products: list[Product],
+            total: float
+    ) -> None:
         self.products = products
         self.total = total
 
+    def to_dict(self) -> dict:
+        return {
+            "products": [product.to_dict() for product in self.products],
+            "total": self.total
+        }
+
+    @staticmethod
+    def from_dict(data: dict) -> "Order":
+        products = [
+            Product.from_dict(item)
+            for item in data["products"]
+        ]
+
+        return Order(
+            products,
+            data["total"]
+        )
+
 
 class User:
-    def __init__(self, username, email, address):
+    def __init__(
+            self,
+            username: str,
+            email: str,
+            address: str
+    ) -> None:
         self.username = username
         self.email = email
         self.address = address
         self.cart = Cart()
         self.orders = []
 
-    def add_order(self, order):
+    def add_order(self, order: Order) -> None:
         self.orders.append(order)
 
-    def view_orders(self):
+    def view_orders(self) -> list:
         return self.orders
 
 class Customer(User):
-    def __init__(self, username, email, address):
+    def __init__(
+            self,
+            username: str,
+            email: str,
+            address: str
+    ) -> None:
         super().__init__(username, email, address)
 
 class Admin(User):
-    def __init__(self, username, email, address):
+    def __init__(
+            self,
+            username: str,
+            email: str,
+            address: str
+    ) -> None:
         super().__init__(username, email, address)
 
-    def add_product(self, product, product_list):
+    def add_product(
+            self,
+            product: Product,
+            product_list: list
+    ) -> None:
         product_list.append(product)
